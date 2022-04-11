86c6a77e8d534b20be0f6128e89a9c8aef574b05
alexymumo <alexymumo2000@gmail.com>
2022-04-11 10:38:03 +0300

    updates

--- app/src/main/java/com/alexmumo/movies/MainActivity.kt
+++ app/src/main/java/com/alexmumo/movies/MainActivity.kt
@@ -3,36 +3,26 @@ package com.alexmumo.movies
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.alexmumo.movies.screens.HomeScreen
 import com.alexmumo.movies.ui.theme.MoviesTheme
 
+@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MoviesTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                HomeScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MoviesTheme {
-        Greeting("Android")
-    }
 }
--- app/src/main/java/com/alexmumo/movies/screens/HomeScreen.kt
+++ app/src/main/java/com/alexmumo/movies/screens/HomeScreen.kt
@@ -1,19 +1,22 @@
 package com.alexmumo.movies.screens
 
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.lazy.rememberLazyGridState
 import androidx.compose.runtime.Composable
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.alexmumo.movies.components.MovieGrid
 import com.alexmumo.presentation.viewmodel.MovieViewModel
 import org.koin.androidx.compose.getViewModel
 
+@ExperimentalFoundationApi
 @Composable
 fun HomeScreen(viewModel: MovieViewModel = getViewModel()) {
     val state = viewModel.movieListState.value
-    val lazyListState = rememberLazyListState()
+    val lazyGridState = rememberLazyGridState()
 
     val movies = state.movieList?.collectAsLazyPagingItems()
 
-    movies?.let {
-
+    movies?.let { items ->
+        MovieGrid(items = items, lazyGridState)
     }
 }
