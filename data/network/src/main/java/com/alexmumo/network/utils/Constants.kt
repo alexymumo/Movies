2b40eb6b17f7fd016b984f7052e643338c4d68de
alexymumo <alexymumo2000@gmail.com>
2022-04-11 11:06:07 +0300

    added base repository

--- data/network/src/main/java/com/alexmumo/network/utils/Constants.kt
+++ data/network/src/main/java/com/alexmumo/network/utils/Constants.kt
@@ -1,6 +1,6 @@
 package com.alexmumo.network.utils
 
 object Constants {
-    const val BASE_URL = "http://alex.com"
+    const val BASE_URL = "https://api.themoviedb.org/3/"
     const val API_KEY = "fd65c1178171ce0d12871600b495fa47"
 }
