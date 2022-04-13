5a67c632f99e2879d7cb700db45efb25da1d6734
alexymumo <alexymumo2000@gmail.com>
2022-04-13 12:30:27 +0300

    update

--- data/network/src/main/java/com/alexmumo/network/api/MovieApi.kt
+++ data/network/src/main/java/com/alexmumo/network/api/MovieApi.kt
@@ -1,6 +1,8 @@
 package com.alexmumo.network.api
 
 import com.alexmumo.network.models.PopularMovieDto
+import com.alexmumo.network.models.RecommendationsDto
+import com.alexmumo.network.models.TopRatedDto
 import com.alexmumo.network.models.UpComingMovieDto
 import com.alexmumo.network.utils.Constants.API_KEY
 import retrofit2.http.GET
@@ -8,21 +10,30 @@ import retrofit2.http.Query
 
 interface MovieApi {
     @GET("/movie/popular")
-    suspend fun getPopularMovies(
+    suspend fun fetPopularMovies(
         @Query("api_key") apiKey: String = API_KEY,
         @Query("page") page: Int = 1,
         @Query("language") language: String = "en"
     ): PopularMovieDto
 
     @GET("/movie/top_rated")
-    suspend fun getTopRatedMovies(
+    suspend fun fetTopRatedMovies(
         @Query("api_key") apiKey: String = API_KEY,
+        @Query("page") page: Int = 1,
         @Query("language") language: String = "en"
-    )
+    ): TopRatedDto
 
     @GET("/movie/upcoming")
-    suspend fun getUpComingMovies(
+    suspend fun fetUpComingMovies(
         @Query("api_key") apiKey: String = API_KEY,
+        @Query("page") page: Int = 1,
         @Query("language") language: String = "en"
     ): UpComingMovieDto
+
+    @GET("/movie/{movie_id}/recommendations")
+    suspend fun fetchMovieRecommendations(
+        @Query("api_key") apiKey: String = API_KEY,
+        @Query("page") page: Int = 1,
+        @Query("language") language: String = "en"
+    ): RecommendationsDto
 }
