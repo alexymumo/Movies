c6bae1e4fef3c961ce2fd81ee3b953c8f13f7a1b
alexymumo <alexymumo2000@gmail.com>
2022-04-18 21:52:08 +0300

    added remoteKeys

--- data/repository/src/main/java/com/alexmumo/repository/datasources/MovieRepositoryImpl.kt
+++ data/repository/src/main/java/com/alexmumo/repository/datasources/MovieRepositoryImpl.kt
@@ -12,7 +12,10 @@ import com.alexmumo.domain.repositories.MovieRepository
 import com.alexmumo.network.api.MovieApi
 import com.alexmumo.network.utils.BaseRepository
 import com.alexmumo.repository.mediator.MovieRemoteMediator
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 
 @ExperimentalPagingApi
 class MovieRepositoryImpl constructor(
@@ -21,6 +24,16 @@ class MovieRepositoryImpl constructor(
 ) : MovieRepository, BaseRepository() {
     private val movieDao = movieDatabase.movieDao()
     private val _movie = MutableLiveData<List<MovieEntity>>()
+
+    init {
+        _movie.observeForever { movies ->
+            CoroutineScope(Dispatchers.IO).launch {
+                movieDao.saveMovie(movies)
+            }
+        }
+    }
+
+    private suspend fun saveMovie(movies: List<MovieEntity>) = movieDao.saveMovie(movies)
 
     override suspend fun fetchAllMovies(category: String): Flow<PagingData<Movie>> {
         val pagingConfig = PagingConfig(
@@ -43,5 +56,5 @@ class MovieRepositoryImpl constructor(
             pagingSourceFactory = pagingSource
         ).flow
     }
-    private suspend fun saveMovie(movies: List<MovieEntity>) = movieDao.saveMovie(movies)
 }
+
--- data/repository/src/main/java/com/alexmumo/repository/mediator/MovieRemoteMediator.kt
+++ data/repository/src/main/java/com/alexmumo/repository/mediator/MovieRemoteMediator.kt
@@ -5,17 +5,97 @@ import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
 import com.alexmumo.cache.db.MovieDatabase
+import com.alexmumo.cache.entity.RemoteKey
 import com.alexmumo.domain.models.Movie
 import com.alexmumo.network.api.MovieApi
 
 @ExperimentalPagingApi
-class MovieRemoteMediator constructor(
+class MovieRemoteMediator(
     private val category: String,
     private val movieApi: MovieApi,
     private val movieDatabase: MovieDatabase
 ) : RemoteMediator<Int, Movie>() {
 
-    override suspend fun load(loadType: LoadType, state: PagingState<Int, Movie>): MediatorResult {
-        TODO("Not yet implemented")
+    private val remoteKey = movieDatabase.remoteDao()
+    private val movieDao = movieDatabase.movieDao()
+
+
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, Movie>
+    ): MediatorResult {
+        val page: Int = when(loadType) {
+
+            LoadType.REFRESH -> {
+                val remoteKeys = getRemoteKeyClosestToCurrentPosition(state)
+                remoteKeys?.nextKey?.minus(1)?: 1
+            }
+            LoadType.PREPEND -> {
+                val remoteKeys = getRemoteKeyForFirstItem(state)
+                val prevKey = remoteKeys?.prevKey
+                    ?: return MediatorResult.Success(endOfPaginationReached = true)
+                prevKey
+            }
+            LoadType.APPEND -> {
+                val remoteKeys = getRemoteKeyForLastItem(state)
+                val nextKey = remoteKeys?.nextKey
+                ?: return MediatorResult.Success(endOfPaginationReached = true)
+                nextKey
+            }
+        }
+    }
+
+    private suspend fun getRemoteKeyForFirstItem(state: PagingState<Int, Movie>): RemoteKey? {
+        return state.pages.firstOrNull { it.data.isNullOrEmpty() }?.data?.firstOrNull()
+            ?.let { movie ->
+                movieDatabase.remoteDao().fetchRemoteKey(movie_id = movie.id!!)
+            }
+    }
+
+    private suspend fun getRemoteKeyForLastItem(state: PagingState<Int, Movie>): RemoteKey? {
+        return state.pages.lastOrNull { it.data.isNullOrEmpty() }?.data?.lastOrNull()
+            ?.let { movie ->
+                movieDatabase.remoteDao().fetchRemoteKey(movie_id = movie.id!!)
+            }
+
+    }
+
+    private suspend fun getRemoteKeyClosestToCurrentPosition(state: PagingState<Int, Movie>): RemoteKey? {
+        return state.anchorPosition?.let { position ->
+            state.closestItemToPosition(position)?.let { movie ->
+                movieDatabase.remoteDao().fetchRemoteKey(movie_id = movie.id!!)
+            }
+        }
     }
 }
+
+/*
+  *
+  * PagingState -> contains information about the pages loaded, most recently accessed index and PagingConfig object
+  * LoadType -> indicates the load type REFRESH, APPEND, PREPEND
+  * load() performs the following steps
+  * 1. Determine which page to load from the network depending on the load type
+  * 2. Trigger network request
+  * 3. Perform actions depending on the outcome of the load operations
+  *
+  * */
+
+/*
+* PAGE FROM NETWORK AND DATABASE
+* RemoteMediator acts as signal from the paging from
+* the paging library when the app has run out of cached data
+* The signal can be used to load additional data from the network and store it in
+* the local database where PagingSource can load it and provide it to the UI for display
+* When additional data is needed Paging library calls the load() method f
+* LoadType informs the the RemoteMediator whether it needs to refresh the existing data
+* or fetch additional data that needs to be appended or prepended to existing list
+* PagingSource implementation is passed to the Pager through pagingSourceFactory parameter
+* As new data is required by the UI the Pager calls the load() from PagingSource and return
+* stream of PagingData objects that encapsulates the new data
+* Each PagingData object is cached in the ViewModel before being sent to the UI
+* RemoteMediator changes the data flow
+* If data is exhausted Paging triggers the RemoteMediator to load new data from network
+* RemoteMediator stores the new data in the local database
+* load() - responsible for updating the backing dataset and invalidating the PagingSource
+*
+* */
