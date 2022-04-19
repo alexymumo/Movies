7a4cce17961b52c8197339e512736476c28c1758
alexymumo <alexymumo2000@gmail.com>
2022-04-19 21:15:34 +0300

    addded repository module

--- data/repository/src/main/java/com/alexmumo/repository/di/RepositoryModule.kt
+++ data/repository/src/main/java/com/alexmumo/repository/di/RepositoryModule.kt
@@ -1,6 +1,13 @@
 package com.alexmumo.repository.di
 
+import androidx.paging.ExperimentalPagingApi
+import com.alexmumo.domain.repositories.MovieRepository
+import com.alexmumo.repository.datasources.MovieRepositoryImpl
 import org.koin.dsl.module
 
+@ExperimentalPagingApi
 val repositoryModule = module {
+    single<MovieRepository> {
+        MovieRepositoryImpl(movieApi = get(), movieDatabase = get())
+    }
 }
