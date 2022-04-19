67e435b5f1e37d8da247e4bef02cfeaab19dbf46
alexymumo <alexymumo2000@gmail.com>
2022-04-20 00:53:41 +0300

    composables

--- app/src/main/java/com/alexmumo/movies/screens/home/HomeScreen.kt
+++ app/src/main/java/com/alexmumo/movies/screens/home/HomeScreen.kt
@@ -1,13 +1,12 @@
 package com.alexmumo.movies.screens.home
 
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import org.koin.androidx.compose.getViewModel
 
 @Composable
-fun HomeScreen(navController: NavController) {
-    Text(text = "Hello world")
+fun HomeScreen(navController: NavController, viewModel: HomeViewModel = getViewModel()) {
 }
 
 @Preview
--- app/src/main/java/com/alexmumo/movies/screens/popular/PopularScreen.kt
+++ app/src/main/java/com/alexmumo/movies/screens/popular/PopularScreen.kt
@@ -1,16 +1,20 @@
 package com.alexmumo.movies.screens.popular
 
-import androidx.compose.foundation.background
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import androidx.paging.PagingData
+import com.alexmumo.domain.models.Movie
+import com.alexmumo.movies.screens.home.HomeViewModel
+import kotlinx.coroutines.flow.Flow
 
 @Composable
-fun PopularScreen(navController: NavController) {
-    Text(text = "Popular Screen", Modifier.background(color = Color.DarkGray))
+fun PopularScreen(
+    navController: NavController,
+    viewModel: HomeViewModel,
+    movies: Flow<PagingData<Movie>>
+) {
+
 }
 
 @Preview
