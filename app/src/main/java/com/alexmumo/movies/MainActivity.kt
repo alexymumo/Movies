1015926758c33ba8215fffdd4f1713c76559c9e5
alexymumo <alexymumo2000@gmail.com>
2022-04-19 08:32:46 +0300

    added splash screen

--- app/src/main/java/com/alexmumo/movies/MainActivity.kt
+++ app/src/main/java/com/alexmumo/movies/MainActivity.kt
@@ -3,21 +3,23 @@ package com.alexmumo.movies
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.alexmumo.movies.screens.HomeScreen
 import com.alexmumo.movies.ui.theme.MoviesTheme
 
-@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MoviesTheme {
-                HomeScreen()
+                Surface(color = MaterialTheme.colors.background) {
+                    HomeScreen()
+                }
             }
         }
     }
--- app/src/main/java/com/alexmumo/movies/navigation/NavigationItem.kt
+++ app/src/main/java/com/alexmumo/movies/navigation/NavigationItem.kt
@@ -7,4 +7,5 @@ sealed class NavigationItem(var route: String, var icon: Int, var title: String)
     object TopRated : NavigationItem("toprated", R.drawable.ic_toprated, "TopRated")
     object Upcoming : NavigationItem("upcoming", R.drawable.ic_upcoming, "Upcoming")
     object Favorite : NavigationItem("favorite", R.drawable.ic_favorite, "Favorite")
+    object Splash : NavigationItem("splash", R.drawable.ic_favorite, "Splash")
 }
