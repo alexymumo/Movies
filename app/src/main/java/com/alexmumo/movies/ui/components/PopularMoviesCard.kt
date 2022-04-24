3b630a87678df50c8f9a653bd6c4347c6df3a87e
alexymumo <alexymumo2000@gmail.com>
2022-04-25 00:18:29 +0300

    updated homescreen

--- app/src/main/java/com/alexmumo/movies/ui/components/PopularMoviesCard.kt
+++ app/src/main/java/com/alexmumo/movies/ui/components/PopularMoviesCard.kt
@@ -1,19 +1,19 @@
 package com.alexmumo.movies.ui.components
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.animation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import coil.request.ImageRequest
@@ -23,26 +23,21 @@ import com.skydoves.landscapist.coil.CoilImage
 
 @Composable
 fun PopularMoviesCard(
-    onClickItem: (Movie) -> Unit,
-    movie: Movie,
     movieString: String,
     modifier: Modifier = Modifier
 ) {
     Card(
         modifier = modifier
             .height(300.dp)
-            .width(200.dp)
-            .clickable { onClickItem(movie) },
+            .width(200.dp),
         elevation = 4.dp,
         shape = RoundedCornerShape(8.dp)
     ) {
         ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-            val (movieImage, movieTitle) = createRefs()
 
             CoilImage(
                 imageRequest = ImageRequest
                     .Builder(LocalContext.current)
-                    .data(movieString)
                     .crossfade(true)
                     .build(),
                 alignment = Alignment.Center,
@@ -61,7 +56,67 @@ fun PopularMoviesCard(
                 },
                 contentScale = ContentScale.Crop
             )
-            Box() {
+        }
+    }
+}
+
+@Composable
+fun MovieInfo(
+    movie: Movie,
+    modifier: Modifier = Modifier,
+    alignment: Alignment.Horizontal = Alignment.Start
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        horizontalAlignment = alignment
+    ) {
+        Text(
+            text = movie.releaseDate!!,
+            fontWeight = FontWeight.Bold,
+            style = MaterialTheme.typography.h6
+        )
+    }
+}
+
+@Composable
+fun PopularMoviesUI(
+    movie: Movie,
+    modifier: Modifier = Modifier
+) {
+    val density = LocalDensity.current
+    AnimatedVisibility(
+        visible = true,
+        enter = slideInVertically {
+            with(density) { -40.dp.roundToPx() }
+        } + expandVertically(
+            expandFrom = Alignment.Bottom
+        ) + fadeIn(
+            initialAlpha = 0.3f
+        ),
+        exit = slideOutVertically() + shrinkHorizontally() + fadeOut()
+    ) {
+        Card(
+            modifier = modifier
+                .animateContentSize()
+                .padding(8.dp),
+            shape = RoundedCornerShape(8.dp),
+            elevation = 8.dp
+        ) {
+            Row {
+                PopularMoviesCard(
+                    movieString = movie.backdropPath!!,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                MovieInfo(
+                    movie = movie,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp)
+                )
             }
         }
     }
